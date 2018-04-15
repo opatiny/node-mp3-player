7767cbe2e06e9044231390c0170a1d80aed564d6
Oceane Patiny <oceane@patiny.com>
2018-04-15 13:28:25 +0000

    add shutdown test

--- src/i2c/tests/shutdown.js
+++ src/i2c/tests/shutdown.js
@@ -5,14 +5,27 @@ const CardReader = require('../cardReader.js');
 
 const cardReader = new CardReader(i2c);
 
+const Shutdown = require('shutdown');
+
+
+
 read();
 
 
 async function read() {
+    let lastZero=Date.now();
     while (true) {
         let status=cardReader.status();;
         console.log(status);
         await delay(500);
+	if (status.switchState===0) {
+		if ((Date.now()-lastZero) > 10000) {
+			Shutdown.shutdownGracefully();
+		}
+	} else {
+		lastZero=Date.now();
+
+	}
     }
 }
 
