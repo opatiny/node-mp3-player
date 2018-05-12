89e678ff20bc7c5f3adf5e1ffd8bcd634cfa4a84
Océane Patiny <oceane@patiny.com>
2018-05-12 19:13:25 +0200

    add a test function to display toc

---
+++ src/util/tests/showToc.js
@@ -0,0 +1,10 @@
+// test ledDisplay: show tune information, author and title
+
+'use strict';
+
+const debug = require('debug')('util:test/displayInfo'); // debug library
+
+const toc = require('../loadTOC');
+
+console.log(Object.keys(toc));
+
