45d032a72a5406796e972b0a33a65468bcd2a41f
Océane Patiny <oceane@patiny.com>
2018-05-12 16:06:26 +0200

    test hole project

--- src/i2c/tests/display.js
+++ src/i2c/tests/display.js
@@ -8,21 +8,22 @@ const delay = require('delay');
 
 // require Display and create new instance
 const Display = require('../display.js');
+
 const display = new Display(i2c);
 
 test();
 
 async function test() {
-    for (let i=0; i<10; i++) {
-        for (let line = 0; line<3; line++) {
-            display.setIntensity(Math.floor(Math.random()*500)); // Math.floor rounds to integer under
-            display.on(line);
-            await delay(100);
-            display.off(line);
-            await delay(100);
-        }
+  for (let i = 0; i < 10; i++) {
+    for (let line = 0; line < 3; line++) {
+      display.setIntensity(Math.floor(Math.random() * 500)); // Math.floor rounds to integer under
+      display.on(line);
+      await delay(100);
+      display.off(line);
+      await delay(100);
     }
+  }
 }
 
 display.setText('ABCDEFGHIJKL');
-await delay(100);
+
---
+++ src/i2c/tests/displayOff.js
@@ -0,0 +1,23 @@
+// test ledDisplay: turn everything off
+'use strict';
+
+// require libraries
+const i2c = require('i2c-bus').openSync(1);
+const delay = require('delay');
+
+// require Display and create new instance
+const Display = require('../display.js');
+
+const display = new Display(i2c);
+
+allOff();
+
+async function allOff() {
+  for (let line = 0; line < 3; line++) {
+    display.off(line);
+    await delay(100);
+  }
+}
+
+display.setText('ABCDEFGHIJKL');
+
