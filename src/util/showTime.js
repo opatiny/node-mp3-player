dafc63fba038052f6ba08cc07f683605672da649
Luc Patiny <luc@patiny.com>
2018-05-12 20:36:20 +0000

    add playlist counter

--- src/util/showTime.js
+++ src/util/showTime.js
@@ -8,7 +8,8 @@ const debug = require('debug')('util:showTime'); // debug library
 module.exports = async function showTime(context) {
   if (context.item && context.item.getCurrentTime) {
     let time = await context.item.getCurrentTime();
-    context.display.setText((`${context.info.length - Math.floor(time)} s`).padEnd(8), { line: 2 });
+
+    context.display.setText((`${context.info.length - Math.floor(time)} s`).padEnd(6)+String(context.playlist.length).padStart(2), { line: 2 });
     debug(time);
   }
 };
